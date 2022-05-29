cc8ad2b8bbb32643514f959596985a189ab22bf1
rick-cm <rick_mmartins@hotmail.com>
2022-05-28 21:00:02 -0300

    primeiro commit

---
+++ migration-flywaydb1/migration-flywaydb1/src/main/java/com/rcmto/migrationflywaydb1/MigrationFlywaydb1Application.java
@@ -0,0 +1,13 @@
+package com.rcmto.migrationflywaydb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationFlywaydb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationFlywaydb1Application.class, args);
+	}
+
+}
---
+++ migration-flywaydb1/migration-flywaydb1/src/test/java/com/rcmto/migrationflywaydb1/MigrationFlywaydb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcmto.migrationflywaydb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationFlywaydb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
